daadb1cfbea8d4f4184aedcde098a9a00d832944
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-07 23:29:35 +0530

    Solved mystry of ASP.NET MVC vs ASP.NET MVC Framework

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,36 +5,56 @@ namespace CineComplex_MVC.Controllers
 {
     public class HelloWorldController : Controller
     {
-        
-
         // GET: /HelloWorld
-        public string Index()
-        {
-            return "This is my <b>Default</b> action.";
-        }
-
-        // GET: /HelloWorld/Welcome
-        [HttpGet("HelloWorld/Welcome")]
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
-
-        // GET: /HelloWorld/Welcome?name=someStringValue?numTimes=someIntegerValue
-        [HttpGet("HelloWorld/Welcome/{name}/{numTimes}")]
-        public string Welcome(string name, int numTimes = 1)
-        {
-            return HttpUtility.HtmlEncode($"This is {name} clicks {numTimes} times");
-        }
-
-    }
-}
-
-
-
-// GET: /HelloWorld
         //public IActionResult Index()
         //{
         //    return View();
         //}
+
+        // GET: /HelloWorld
+        public IActionResult Index()
+        {
+            string htmlContent = "This is my <b>Default</b> action in custome Index().";
+            return Content(htmlContent, "text/html");
+        }
+
+        // GET: /HelloWorld/Welcome
+        //[HttpGet("HelloWorld/Welcome")]
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        // GET: /HelloWorld/Welcome
+        [HttpGet("HelloWorld/Welcome")]
+        public IActionResult Welcome()
+        {
+            string htmlContent = "This is the Welcome action method...";
+            return Content(htmlContent, "text/html");
+        }
+    }
+}
+
+        /*
+         * Method overloading is a fundamental concept in many programming languages, allowing for multiple methods with the same name 
+         * but different parameters. However, in the context of ASP.NET MVC (and web frameworks in general), method overloading can sometimes 
+         * be tricky due to how routing and HTTP requests are handled.
+         * 
+         * In traditional method overloading, the method signatures (parameters) distinguish between different versions of a method. 
+         * In ASP.NET MVC, routing maps URLs to specific controller actions, and relying on method overloading alone can lead to route conflicts or ambiguities, 
+         * especially when dealing with HTTP GET requests.
+         * 
+         */
+
+        // GET: /HelloWorld/Welcome?name=someStringValue?numTimes=someIntegerValue
+        //[HttpGet("HelloWorld/Welcome/{name}/{numTimes}")]
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlEncode($"This is {name} clicks {numTimes} times");
+        //}
+
+
+
+
+
 
