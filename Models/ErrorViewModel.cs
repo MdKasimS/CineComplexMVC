f1ca64a99c69e69844f631cde9302ce4eaac5326
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-07 20:41:18 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineComplex_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
