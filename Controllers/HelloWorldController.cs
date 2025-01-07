09873237f61d2f2b64d9ae0970dace3cd5d6eaaf
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-01-07 22:45:08 +0530

    Update with correct routing path in attributes

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,10 +5,7 @@ namespace CineComplex_MVC.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        
 
         // GET: /HelloWorld
         public string Index()
@@ -17,14 +14,14 @@ namespace CineComplex_MVC.Controllers
         }
 
         // GET: /HelloWorld/Welcome
-        [HttpGet("Home/Welcome")]
+        [HttpGet("HelloWorld/Welcome")]
         public string Welcome()
         {
             return "This is the Welcome action method...";
         }
 
         // GET: /HelloWorld/Welcome?name=someStringValue?numTimes=someIntegerValue
-        [HttpGet("Home/Welcome/{name}/{numTimes}")]
+        [HttpGet("HelloWorld/Welcome/{name}/{numTimes}")]
         public string Welcome(string name, int numTimes = 1)
         {
             return HttpUtility.HtmlEncode($"This is {name} clicks {numTimes} times");
@@ -33,4 +30,11 @@ namespace CineComplex_MVC.Controllers
     }
 }
 
+
+
+// GET: /HelloWorld
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
